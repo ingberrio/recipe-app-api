9298a98ea8fe97a5cb4a6a5f3e69a4eaea216ed8
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-09-30 12:58:26 -0500

    Add function Add & Substrac

---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_number(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+    
+    def test_substrac_number(self):
+        """Test substraction numbers together"""
+        res = calc.substrac(15, 10)
+
+        self.assertEqual(res, 5)
