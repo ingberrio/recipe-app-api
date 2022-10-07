0a6abc37729289f00376946a4b742f24cf819420
Eduard Berrio <eduardberrio@MacBook-Pro-de-Eduard-2.local>
2022-10-07 16:16:09 -0500

    Created Recipe API with test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,8 +1,6 @@
 """
 Django admin customization.
 """
-from dataclasses import field
-import email
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -11,11 +9,12 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    """Define the admin page for users."""
+    """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
@@ -40,9 +39,10 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Recipe)
